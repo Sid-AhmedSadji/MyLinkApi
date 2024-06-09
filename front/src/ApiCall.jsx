c050aae1479ffb784f2e7f9c12dc1752f20f8da2
Sid-AhmedSadji <sid-ahmed_sadji@hotmail.com>
2024-06-09 21:24:31 +0200

    first commit

--- front/src/ApiCall.jsx
+++ front/src/ApiCall.jsx
@@ -5,27 +5,27 @@ import React from 'react';
 
 class Api {
     
-    static url = "http://localhost:7000" ; 
+    static url = "https://mylinkapi.onrender.com/Api" ; 
 
     static async getLinks (query) {
-        const reponse = await axios.get(Api.url + "/", {params: query});
+        const reponse = await axios.get(Api.url, {params: query});
         return reponse.data;
 
     }
 
     static async updateLink(id, query) {
         console.log(id, query)
-        const reponse = await axios.put(Api.url + "/",null, {params : { id, query }});
+        const reponse = await axios.put(Api.url,null, {params : { id, query }});
         return reponse.data;
     }
 
     static async deleteLink(_id) {
-        const reponse = await axios.delete(Api.url + "/", {params : { _id }});
+        const reponse = await axios.delete(Api.url, {params : { _id }});
         return reponse.data;
     }
 
     static async postLink(query) {
-        const reponse = await axios.post(Api.url + "/", null,{params :query});
+        const reponse = await axios.post(Api.url, null,{params :query});
         return reponse.data;
     }
 
